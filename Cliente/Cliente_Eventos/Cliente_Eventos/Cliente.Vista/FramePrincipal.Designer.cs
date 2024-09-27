f47dbd8dee747d4ce6dc4b11b2d118e977b3011b
DSebs <sebastian.diaz@estudiantesunibague.edu.co>
2024-09-27 00:51:39 -0500

    First version of the client, MainFrame styles

---
+++ Cliente/Cliente_Eventos/Cliente_Eventos/Cliente.Vista/FramePrincipal.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Cliente_Eventos
+{
+    partial class FramePrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FramePrincipal));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.conciertoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.agregarConciertoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buscaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarConciertoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actualizarConciertoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listarConciertosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.conciertoToolStripMenuItem,
+            this.ayudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(78, 9);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(327, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuOpcion";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.AutoToolTip = true;
+            this.archivoToolStripMenuItem.BackColor = System.Drawing.Color.White;
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.archivoToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(107)))), ((int)(((byte)(32)))));
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(103, 29);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            this.archivoToolStripMenuItem.Click += new System.EventHandler(this.archivoToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 30);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // conciertoToolStripMenuItem
+            // 
+            this.conciertoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.agregarConciertoToolStripMenuItem,
+            this.buscaToolStripMenuItem,
+            this.eliminarConciertoToolStripMenuItem,
+            this.actualizarConciertoToolStripMenuItem,
+            this.listarConciertosToolStripMenuItem});
+            this.conciertoToolStripMenuItem.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conciertoToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(107)))), ((int)(((byte)(32)))));
+            this.conciertoToolStripMenuItem.Name = "conciertoToolStripMenuItem";
+            this.conciertoToolStripMenuItem.Size = new System.Drawing.Size(123, 29);
+            this.conciertoToolStripMenuItem.Text = "Concierto";
+            // 
+            // agregarConciertoToolStripMenuItem
+            // 
+            this.agregarConciertoToolStripMenuItem.Name = "agregarConciertoToolStripMenuItem";
+            this.agregarConciertoToolStripMenuItem.Size = new System.Drawing.Size(292, 30);
+            this.agregarConciertoToolStripMenuItem.Text = "Agregar Concierto";
+            // 
+            // buscaToolStripMenuItem
+            // 
+            this.buscaToolStripMenuItem.Name = "buscaToolStripMenuItem";
+            this.buscaToolStripMenuItem.Size = new System.Drawing.Size(292, 30);
+            this.buscaToolStripMenuItem.Text = "Buscar Concierto";
+            this.buscaToolStripMenuItem.Click += new System.EventHandler(this.buscaToolStripMenuItem_Click);
+            // 
+            // eliminarConciertoToolStripMenuItem
+            // 
+            this.eliminarConciertoToolStripMenuItem.Name = "eliminarConciertoToolStripMenuItem";
+            this.eliminarConciertoToolStripMenuItem.Size = new System.Drawing.Size(292, 30);
+            this.eliminarConciertoToolStripMenuItem.Text = "Eliminar Concierto";
+            // 
+            // actualizarConciertoToolStripMenuItem
+            // 
+            this.actualizarConciertoToolStripMenuItem.Name = "actualizarConciertoToolStripMenuItem";
+            this.actualizarConciertoToolStripMenuItem.Size = new System.Drawing.Size(292, 30);
+            this.actualizarConciertoToolStripMenuItem.Text = "Actualizar Concierto";
+            // 
+            // listarConciertosToolStripMenuItem
+            // 
+            this.listarConciertosToolStripMenuItem.Name = "listarConciertosToolStripMenuItem";
+            this.listarConciertosToolStripMenuItem.Size = new System.Drawing.Size(292, 30);
+            this.listarConciertosToolStripMenuItem.Text = "Listar Conciertos";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.acercaDeToolStripMenuItem});
+            this.ayudaToolStripMenuItem.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ayudaToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(107)))), ((int)(((byte)(32)))));
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(93, 29);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(217, 30);
+            this.acercaDeToolStripMenuItem.Text = "Acerca de ...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(89)))), ((int)(((byte)(165)))));
+            this.label1.Location = new System.Drawing.Point(70, 274);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(359, 44);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "EventManagement";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(145, 70);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(204, 201);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FramePrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(496, 338);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FramePrincipal";
+            this.Text = "Eventos";
+            this.Load += new System.EventHandler(this.FramePrincipal_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem conciertoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agregarConciertoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buscaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminarConciertoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem actualizarConciertoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listarConciertosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Cliente/Cliente_Eventos/Cliente_Eventos/Cliente.Vista/FramePrincipal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cliente_Eventos
+{
+    public partial class FramePrincipal : Form
+    {
+        public FramePrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void buscaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void archivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FramePrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
